c0f68ea4852dcc8f5c179eebd85b0af9be1ad134
Adão Biotto <adao.biotto@southsystem.com.br>
2024-07-08 16:28:45 -0300

    jogoteca logout wip

--- jogoteca.py
+++ jogoteca.py
@@ -1,51 +1,41 @@
-from flask import Flask, render_template, request, redirect, url_for, flash,session
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import mysql.connector
 from wtforms import StringField, EmailField, PasswordField, validators
 from flask_wtf import FlaskForm
 from bd import conecta_no_banco_de_dados
 
-
-
-
 app = Flask(__name__)
 app.secret_key = 'sua_chave_secreta'  # Necessária para usar flash messages e cookies
 
-
 @app.route('/validalogin', methods=['POST', 'GET'])
 def login():
-  
-  email = request.form.get('email')
-  senha = request.form.get('senha')
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    senha = request.form.get('senha')
 
-  # Validar as credenciais
-  bd =conecta_no_banco_de_dados()
-            # A linha bd = conecta_no_banco_de_dados() tenta estabelecer uma conexão com o banco de dados usando a função conecta_no_banco_de_dados().
-            # Essa função, é responsável por lidar com os detalhes da conexão.
-            
-  cursor = bd.cursor()
-  cursor.execute("""
-            SELECT *
-            FROM contatos
-            WHERE email = %s AND senha = %s;
-        """, (email, senha,))
-  usuario = cursor.fetchone()
-  cursor.close()
-  bd.close()
+    # Validar as credenciais
+    bd = conecta_no_banco_de_dados()
+    cursor = bd.cursor()
+    cursor.execute("""
+        SELECT *
+        FROM contatos
+        WHERE nome = %s AND email = %s AND senha = %s;
+    """, (nome, email, senha))
+    usuario = cursor.fetchone()
+    cursor.close()
+    bd.close()
 
-  if usuario:
-  # Login bem-sucedido
-   session['usuario_id'] = usuario[0]
-   session['usuario_nome'] = usuario[1]
-  #  nome_capturado = session.get('usuario_nome')
-   print('usuario_nome')
-   return redirect(url_for('inicio'))
-  else:
-    # Login inválido
-    
-    return redirect(url_for('pagina_login'))
-  
+    if usuario:
+        # Login bem-sucedido
+        session['usuario_id'] = usuario[0]
+        session['usuario_nome'] = usuario[1]
+        flash(f"{session['usuario_nome']} logado com sucesso!")
+        return redirect(url_for('novo'))
+    else:
+        # Login inválido
+        flash('Usuário não logado!')
+        return redirect(url_for('pagina_login'))
 
-  
 class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
@@ -57,11 +47,10 @@ jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
 jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 lista = [jogo1, jogo2, jogo3]
 
-@app.route('/inicio', methods=['GET',])
+@app.route('/inicio', methods=['GET'])
 def index():
-    
-      usuario_nome = session.get('usuario_nome')
-      return render_template('lista.html', titulo='Jogos', jogos=lista, usuario=usuario_nome) ## aqui nao funciona ainda na proxima feature sera implementado o nome do usuario logado de maneira correta
+    usuario_nome = session.get('usuario_nome')
+    return render_template('lista.html', titulo='Jogos', jogos=lista, usuario=usuario_nome)
 
 @app.route('/novo')
 def novo():
@@ -74,7 +63,7 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return redirect(url_for('index'))  
+    return redirect(url_for('index'))
 
 @app.route('/cadastrar', methods=['GET', 'POST'])
 def cadastrar():
@@ -91,59 +80,34 @@ def cadastrar():
         return render_template('cadastro.html')
     if not senha:
         flash('A senha é obrigatória.')
-        return render_template('cadastro.html')        
-    # Abre uma conexão com o banco de dados MySQL usando as credenciais especificadas.
-    bd =conecta_no_banco_de_dados()
-                # A linha bd = conecta_no_banco_de_dados() tenta estabelecer uma conexão com o banco de dados usando a função conecta_no_banco_de_dados().
-                # Essa função, é responsável por lidar com os detalhes da conexão.
-                
+        return render_template('cadastro.html')
+
+    bd = conecta_no_banco_de_dados()
     cursor = bd.cursor()
-            
-    # Executa uma consulta SQL para verificar se já existe um usuário com o email informado.
     cursor.execute("""
         SELECT COUNT(*)
         FROM contatos
         WHERE email = %s;
-        """, (email,))
-            
-    # Obtém o resultado da consulta (o número de usuários encontrados).
+    """, (email,))
     existe = cursor.fetchone()[0]
-            
-    # Fecha o cursor e a conexão com o banco de dados.
     cursor.close()
     bd.close()
-    # Se a consulta anterior retornou um valor maior que zero (usuário já existe), renderiza o template 'cadastro.html' novamente, passando uma mensagem de erro.
+
     if existe > 0:
         flash('Email já cadastrado')
         return render_template('cadastro.html')
     else:
         try:
-        # Reabre a conexão com o banco.
-            bd =conecta_no_banco_de_dados()
-                    # A linha bd = conecta_no_banco_de_dados() tenta estabelecer uma conexão com o banco de dados usando a função conecta_no_banco_de_dados().
-                    # Essa função, é responsável por lidar com os detalhes da conexão.
-                    
+            bd = conecta_no_banco_de_dados()
             cursor = bd.cursor()
-                        
-            # Prepara a consulta SQL de inserção.
             sql = 'INSERT INTO contatos (nome, email, senha) VALUES (%s, %s, %s)'
             values = (nome, email, senha)
-
-            # Executa a consulta com os valores obtidos do formulário.
-            cursor.execute(sql, list(values))
-            # Fecha o cursor e confirma a transação (commit).
-            cursor.close()
+            cursor.execute(sql, values)
             bd.commit()
-
-            # Redireciona para a página inicial.
+            cursor.close()
             return redirect(url_for('login'))
-            # Se ocorrer um erro de banco de dados, renderiza o template 'cadastro.html' novamente, passando a mensagem de erro SQL.
-        except bd.connector.Error as e:   
-                return render_template('cadastro', error=str(e)) 
-            
-          # Se o usuário não existe, tenta inserir o novo usuário no banco de dados:   
-    
-  
+        except mysql.connector.Error as e:
+            return render_template('cadastro.html', error=str(e))
 
 @app.route('/', methods=['GET', 'POST'])
 def pagina_login():
@@ -163,11 +127,13 @@ def pagina_login():
 
         cursor = bd.cursor()
         try:
-            cursor.execute("SELECT * FROM contatos WHERE email=%s AND senha=%s", (email, senha))
+            cursor.execute("SELECT * FROM contatos WHERE nome=%s AND email=%s AND senha=%s", (nome, email, senha))
             user = cursor.fetchone()
             if user:
+                session['usuario_id'] = user[0]
+                session['usuario_nome'] = user[1]
                 flash("Login realizado com sucesso.")
-                return redirect(url_for('index'))  
+                return redirect(url_for('index'))
             else:
                 flash("Usuário ou senha incorretos.")
         except mysql.connector.Error as err:
@@ -177,6 +143,12 @@ def pagina_login():
             bd.close()
 
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario_nome', None)
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('pagina_login'))
 
 if __name__ == '__main__':
     db_conn = conecta_no_banco_de_dados()
